be0a84bd8592c77b4a4aa7674037a4b4fa5bcd9f
satinder <satindersingh472@gmail.com>
2022-07-16 16:19:05 -0600

    working

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let menu_items = [
+    `https://images.unsplash.com/photo-1657299141998-2aba7e9bdebb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=698&q=80`,
+    `https://images.unsplash.com/photo-1657915255369-979d2339884b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80`,
+    `https://images.unsplash.com/photo-1657977197667-12a0555dfef4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`,
+    `https://images.unsplash.com/photo-1657664065994-5e257c88b7f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1083&q=80`,
+    `https://images.unsplash.com/photo-1654036249671-134ea63bc3c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`,
+    `https://images.unsplash.com/photo-1657963087977-cf2283e1b0cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`,
+    `https://images.unsplash.com/photo-1657969956287-fc849f62e99c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`,
+    `https://images.unsplash.com/photo-1657299142014-34b66b73e68e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80`,
+    `https://images.unsplash.com/photo-1657818795488-7942b45b04b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80`,
+    `https://images.unsplash.com/photo-1657811763824-8f42e2d5c657?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60`
+];
+
+ let menu = document.getElementById(`unique_id`);
+ let counter =0;
+ for(counter= 0; counter < menu_items.length; counter++){
+    menu.insertAdjacentHTML(`beforeEnd`, `<img src ="${menu_items[counter]}"/>`);
+ }
+let button = document.getElementById(`unique_button`);
+button[`outerHTML`] = ``;
--- readMe.md
+++ readMe.md
@@ -1,1 +1,23 @@
 This is a menu_array_practice project
+
+3. The Menu
+3.1. Create a variable called menu_items (array of strings)
+3.2. Fill the array with 10 strings that are valid external image 
+sources
+3.3. Create a section in your HTML with a unique id
+3.4. Create a button in your HTML with a unique id
+3.5. Write JS code that will achieve the following:
+3.5.1. Go through the event listening steps to react to the button 
+being clicked
+3.5.2. When the button is clicked, the event function will insert a 
+new img tag onto the page getting the src from the array made 
+earlier.
+3.5.3. After the images are added, the button should be deleted from 
+the page
+3.6. Modify the array to now be an array of objects with the following 
+key-value pairs:
+3.6.1. title (string)
+3.6.1. image_source (string)
+3.6.1. price (number)
+3.7 Modify the event function to now insert a div tag that contains an 
+img tag, an h2 tag for the title and a p tag for the price
